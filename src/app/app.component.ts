a93906a39282b9237567eade1c1e28ba531b4bfb
Jared Lutz and Sean McDermott <sean@>
2018-04-30 16:19:02 -0700

    forgot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 //very cool ~
 @Component({
   selector: 'crazy',
@@ -12,7 +13,9 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  firstTask ={
-    description: "Finish weekend Angular homework for Epicodus course"
-  }
+  tasks: Task[] = [
+  new Task('Finish weekend Angular homework for Epicodus course'),
+  new Task('Begin brainstorming possible JavaScript group projects'),
+  new Task('Add README file to last few Angular repos on GitHub')
+  ];
 }
