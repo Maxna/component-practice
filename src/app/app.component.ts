255d4cea34341add4a34aac9b35d0168decec4ae
Jared Lutz and Sean McDermott <sean@>
2018-04-30 09:59:47 -0700

    set up files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+//very cool ~
 @Component({
-  selector: 'app-root',
+  selector: 'crazy',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  firstTask ={
+    description: "Finish weekend Angular homework for Epicodus course"
+  }
 }
